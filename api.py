66900baf4e7c909a22e3fff1a95d43c0b3cdc2e9
Rambo <ramborigs90@gmail.com>
2024-10-25 00:24:12 +0000

    chmod 774 after transfer for group rw access

--- api.py
+++ api.py
@@ -58,6 +58,9 @@ def run_rsync(remote_user, remote_host, remote_folder, local_folder):
         result = subprocess.run(rsync_command, capture_output=True, text=True)
         
         if result.returncode == 0:
+            # change permissions to allow group rwx
+            subprocess.run(['chmod', '-R', '774', local_folder], capture_output=True, text=True)
+
             return jsonify({'message': 'Backup completed successfully', 'output': result.stdout}), 200
         else:
             return jsonify({'message': 'Backup failed', 'error': result.stderr}), 500
